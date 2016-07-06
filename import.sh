37e8d95c976d845c17ba62863b87df27a8c66d24
marlon.ruescher <m.ruescher@gds-gmbh.de>
2016-07-06 08:40:57 +0200

    updating script outputs

--- import.sh
+++ import.sh
@@ -8,17 +8,17 @@ sudo rm -R $temp_folder
 mkdir -p $temp_folder
 cd $temp_folder
 
-echo -e "Creating git repository from svn"
+echo -e "== Creating git repository from svn ==\n"
 git svn clone $svn_host_url/$2 ./ --authors-file=../authors-file.txt
-echo -e "... done"
+echo -e "... done\n"
 
-echo -e "Creating new repository in gitlab ... "
+echo -e "== Creating new repository in gitlab ==\n"
 curl -H "Content-Type:application/json" $gitlab_host_url/api/v3/projects?private_token=$gitlab_private_token -d "{ \"name\": \"$1\", \"namespace_id\": \"$namespace_id\" }"
-echo -e "... done"
+echo -e "... done\n"
 
-echo -e "Pushing into new repository ..."
+echo -e "== Pushing into new repository ==\n"
 git push --mirror $gitlab_host_url/$namespace_name/$1.git
-echo -e "... done"
+echo -e "... done\n"
 
 cd ..
 sudo rm -R $temp_folder
