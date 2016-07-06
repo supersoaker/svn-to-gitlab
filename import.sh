3eccdf0e5019d6fca2bed644ed551aff130d7799
marlon.ruescher <m.ruescher@gds-gmbh.de>
2016-07-06 08:09:16 +0200

    added outputs and config.cfg for passing config parameters

--- import.sh
+++ import.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+temp_folder="svn-to-gitlab-temp"
+source ./config.cfg
 
-mkdir -p svn-to-gitlab-temp
-sudo rm -R svn-to-gitlab-temp
-cd svn-to-gitlab-temp
+mkdir -p $temp_folder
+sudo rm -R $temp_folder
+cd $temp_folder
 
-git svn clone https://svn.example.com/ ./ --authors-file=authors-file.txt
+echo -e "Creating git repository from svn"
+git svn clone $svn_host_url/$2 ./ --authors-file=authors-file.txt
+echo -e "... done"
 
-curl -H "Content-Type:application/json" https://git.example.com/api/v3/projects?private_token=XXXXXXXXXXXXXXXXXXXX -d "{ \"name\": \"$2\", \"namespace_id\": \"10\" }"
+echo -e "Creating new repository in gitlab ... "
+curl -H "Content-Type:application/json" $gitlab_host_url/api/v3/projects?private_token=$gitlab_private_token -d "{ \"name\": \"$1\", \"namespace_id\": \"$namespace_id\" }"
+echo -e "... done"
 
-git push --mirror https://git.example.com/XXNAMESPACEXX/$2.git
+echo -e "Pushing into new repository ..."
+git push --mirror $gitlab_host_url/$namespace_name/$2.git
+echo -e "... done"
 
 cd ..
-echo "Finished !!1"
+echo "Finished migrating svn repository to gitlab: $gitlab_host_url/$namespace_name/$2.git"
